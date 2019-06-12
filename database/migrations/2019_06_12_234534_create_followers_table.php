a3d5d0236d2f06957a95e8a7ccec2db835c42824
manojkmishra <manojmishra571980@gmail.com>
2019-06-13 09:57:42 +1000

    followers migration added with extra fields

--- database/migrations/2019_06_12_234534_create_followers_table.php
+++ database/migrations/2019_06_12_234534_create_followers_table.php
@@ -15,7 +15,8 @@ class CreateFollowersTable extends Migration
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->increments('id');
-         
+            $table->integer('user_id')->index();
+            $table->integer('follows_id')->index();
             $table->timestamps();
         });
     }
