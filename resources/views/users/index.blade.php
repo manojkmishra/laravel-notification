6657a49c813c61dec383ed2c31d8e4a7feb84131
manojkmishra <manojmishra571980@gmail.com>
2019-06-13 10:30:10 +1000

    follow button added on /users page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -20,6 +20,28 @@
                             @foreach ($users as $user)
                                 <tr>
                                     <td clphpass="table-text"><div>{{ $user->name }}</div></td>
+                                    @if (Auth::User()->isFollowing($user->id))
+                                        <td>
+                                            <form action="{{url('unfollow/' . $user->id)}}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" id="delete-follow-{{ $user->target_id }}" class="btn btn-danger">
+                                                <i class="fa fa-btn fa-trash"></i>Unfollow
+                                                </button>
+                                            </form>
+                                        </td>
+                                    @else
+                                        <td>
+                                            <form action="{{url('follow/' . $user->id)}}" method="POST">
+                                                {{ csrf_field() }}
+
+                                                <button type="submit" id="follow-user-{{ $user->id }}" class="btn btn-success">
+                                                <i class="fa fa-btn fa-user"></i>Follow
+                                                </button>
+                                            </form>
+                                        </td>
+                                    @endif
                                 </tr>
                             @endforeach
                         </tbody>
