fe8e9d8d7947c5995c7c481bf483461a7d9c5100
manojkmishra <manojmishra571980@gmail.com>
2019-06-13 10:11:30 +1000

    user relation added in follow model

--- app/Follow.php
+++ app/Follow.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     //
+    protected $fillable = ['target_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
