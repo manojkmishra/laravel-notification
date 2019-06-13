9a74fb51b42179278a5b5c6b2124011c0ddc72a2
manojkmishra <manojmishra571980@gmail.com>
2019-06-13 10:27:10 +1000

    follow and unfollow logic added in follow controller

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -16,4 +16,30 @@ class FollowController extends Controller
             'users' => User::where('id', '!=', Auth::id())->get()
         ]);
     }
+    public function follow(User $user)
+    {
+        if (!Auth::user()->isFollowing($user->id)) {
+            // Create a new follow instance for the authenticated user
+            Auth::user()->follows()->create([
+                'target_id' => $user->id,
+            ]);
+
+            return back()->with('success', 'You are now friends with '. $user->name);
+        } else {
+            return back()->with('error', 'You are already following this person');
+        }
+
+    }
+
+    public function unfollow(User $user)
+    {
+        if (Auth::user()->isFollowing($user->id)) {
+            $follow = Auth::user()->follows()->where('target_id', $user->id)->first();
+            $follow->delete();
+
+            return back()->with('success', 'You are no longer friends with '. $user->name);
+        } else {
+            return back()->with('error', 'You are not following this person');
+        }
+    }
 }
