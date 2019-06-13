b7287ea7aab2b00e6d8903770bf67e22f7e9f9eb
manojkmishra <manojmishra571980@gmail.com>
2019-06-13 10:10:18 +1000

    follow relationship added in user model

--- app/User.php
+++ app/User.php
@@ -31,4 +31,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    public function follows() {
+        return $this->hasMany(Follow::class);
+    }
 }
