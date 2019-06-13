0a17e8d7cd1fa5ef12dd7adf0964e45377e081af
manojkmishra <manojmishra571980@gmail.com>
2019-06-13 10:22:51 +1000

    users.index view created

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+
+            <!-- Following -->
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    All Users
+                </div>
+
+                <div class="panel-body">
+                    <table class="table table-striped task-table">
+                        <thead>
+                            <th>User</th>
+                            <th> </th>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td clphpass="table-text"><div>{{ $user->name }}</div></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
